ff1c2b8bd3a77304b136e1d4c23157b9bdc38eaa
Luma Rodrigues <luumarodrigues@hotmail.com>
2024-09-19 18:32:11 -0300

    add merge-sort

--- app/main.go
+++ app/main.go
@@ -2,21 +2,49 @@ package main
 
 import "fmt"
 
-// bubbleSort ordena um slice de inteiros
-func Sort(arr []int) {
-	n := len(arr)
-	for i := 0; i < n-1; i++ {
-		for j := 0; j < n-i-1; j++ {
-			if arr[j] > arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
-			}
+func Sort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	middle := len(arr) / 2
+	left := Sort(arr[:middle])
+	right := Sort(arr[middle:])
+
+	return merge(left, right)
+}
+
+// metodo que faz o merge sort
+func merge(left, right []int) []int {
+	result := []int{}
+	i, j := 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
 		}
 	}
+
+	// Adiciona os elementos restantes
+	for i < len(left) {
+		result = append(result, left[i])
+		i++
+	}
+	for j < len(right) {
+		result = append(result, right[j])
+		j++
+	}
+
+	return result
 }
 
 func main() {
 	arr := []int{64, 34, 25, 12, 22, 11, 90}
 	fmt.Println("Array antes de ordenar:", arr)
-	Sort(arr)
+	arr = Sort(arr)
 	fmt.Println("Array depois de ordenar:", arr)
 }
