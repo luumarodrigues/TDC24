f3db4f468427374e0779d699c3829adbeca97344
Luma Rodrigues <luumarodrigues@hotmail.com>
2024-09-19 18:21:59 -0300

    update files

--- app/main.go
+++ app/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // bubbleSort ordena um slice de inteiros
-func bubbleSort(arr []int) {
+func Sort(arr []int) {
 	n := len(arr)
 	for i := 0; i < n-1; i++ {
 		for j := 0; j < n-i-1; j++ {
@@ -14,42 +14,9 @@ func bubbleSort(arr []int) {
 	}
 }
 
-// metodo que faz o merge sort
-func merge(left, right []int) []int {
-	result := []int{}
-	i, j := 0, 0
-
-	for i < len(left) && j < len(right) {
-		if left[i] < right[j] {
-			result = append(result, left[i])
-			i++
-		} else {
-			result = append(result, right[j])
-			j++
-		}
-	}
-
-	// Adiciona os elementos restantes
-	for i < len(left) {
-		result = append(result, left[i])
-		i++
-	}
-	for j < len(right) {
-		result = append(result, right[j])
-		j++
-	}
-
-	return result
-}
-
 func main() {
 	arr := []int{64, 34, 25, 12, 22, 11, 90}
 	fmt.Println("Array antes de ordenar:", arr)
-	bubbleSort(arr)
+	Sort(arr)
 	fmt.Println("Array depois de ordenar:", arr)
-
-	left := []int{1, 3, 5}
-	right := []int{2, 4, 6}
-	merged := merge(left, right)
-	fmt.Println(merged) // Output: [1 2 3 4 5 6]
 }
--- app/sort_test.go
+++ app/sort_test.go
@@ -7,6 +7,6 @@ import (
 func BenchmarkSort(b *testing.B) {
 	arr := []int{64, 34, 25, 12, 22, 11, 90}
 	for i := 0; i < b.N; i++ {
-		bubbleSort(arr)
+		Sort(arr)
 	}
 }
