ad14dc011fd5906be479a204dcb35520adc852fe
Luma Rodrigues <luumarodrigues@hotmail.com>
2024-09-19 17:57:41 -0300

    update main

--- app/main.go
+++ app/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // bubbleSort ordena um slice de inteiros
 func bubbleSort(arr []int) {
 	n := len(arr)
@@ -14,42 +12,33 @@ func bubbleSort(arr []int) {
 	}
 }
 
-// metodo que faz o merge sort
-func merge(left, right []int) []int {
-	result := []int{}
-	i, j := 0, 0
+// // metodo que faz o merge sort
+// func merge(left, right []int) []int {
+// 	result := []int{}
+// 	i, j := 0, 0
 
-	for i < len(left) && j < len(right) {
-		if left[i] < right[j] {
-			result = append(result, left[i])
-			i++
-		} else {
-			result = append(result, right[j])
-			j++
-		}
-	}
+// 	for i < len(left) && j < len(right) {
+// 		if left[i] < right[j] {
+// 			result = append(result, left[i])
+// 			i++
+// 		} else {
+// 			result = append(result, right[j])
+// 			j++
+// 		}
+// 	}
 
-	// Adiciona os elementos restantes
-	for i < len(left) {
-		result = append(result, left[i])
-		i++
-	}
-	for j < len(right) {
-		result = append(result, right[j])
-		j++
-	}
+// 	// Adiciona os elementos restantes
+// 	for i < len(left) {
+// 		result = append(result, left[i])
+// 		i++
+// 	}
+// 	for j < len(right) {
+// 		result = append(result, right[j])
+// 		j++
+// 	}
 
-	return result
-}
+// 	return result
+// }
 
 func main() {
-	arr := []int{64, 34, 25, 12, 22, 11, 90}
-	fmt.Println("Array antes de ordenar:", arr)
-	bubbleSort(arr)
-	fmt.Println("Array depois de ordenar:", arr)
-
-	left := []int{1, 3, 5}
-	right := []int{2, 4, 6}
-	merged := merge(left, right)
-	fmt.Println(merged) // Output: [1 2 3 4 5 6]
 }
